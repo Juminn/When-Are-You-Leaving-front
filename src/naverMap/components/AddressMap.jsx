55c5fbd5f4e49aa44bc923accfb765120e00ad84
jumin <5216863@gmail.com>
2024-02-23 17:24:13 +0900

    rm regacy code, add error message

--- src/naverMap/components/AddressMap.jsx
+++ src/naverMap/components/AddressMap.jsx
@@ -24,7 +24,6 @@ const AddressMap = () => {
   const endMarkerRef = useRef(null);
 
   const naver = window.naver;
-  //const navermaps = window.naver.maps;
 
   //네이버지도 초기화
   useEffect(() => {
@@ -37,23 +36,13 @@ const AddressMap = () => {
       };
 
       mapRef.current = new naver.maps.Map("map", mapOptions);
-      //setMap(map);
 
       contextMenuWindowRef.current = new naver.maps.InfoWindow({
         disableAnchor: true,
         pixelOffset: new naver.maps.Point(+61, 0),
-        //anchorColor: "#ff0",
       });
 
       mapRef.current.setCursor("pointer");
-
-      //
-      //   naver.maps.Event.addListener(mapRef.current, "click", function (e) {
-      //     const marker = new naver.maps.Marker({
-      //       position: e.coord,
-      //       map: mapRef.current,
-      //     });
-      //   });
     };
 
     initMap();
@@ -61,12 +50,17 @@ const AddressMap = () => {
     // 클린업 함수: 컴포넌트 언마운트 시 리소스 정리
     return () => {
       if (contextMenuWindowRef.current) {
-        contextMenuWindowRef.current.close();
+        mapRef.current.destroy();
       }
 
-      // 모든 이벤트 리스너를 제거합니다.
-      //naver.maps.Event.clearListeners(mapRef.current, "click");
-      // 추가적으로 설정된 리스너가 있다면, 여기에서 clearListeners를 호출합니다.
+      if (startMarkerRef.current) {
+        startMarkerRef.current = null;
+      }
+
+      if (endMarkerRef.current) {
+        endMarkerRef.current = null;
+        //endMarkerRef.current.setMap(null);
+      }
     };
   }, []);
 
@@ -76,7 +70,7 @@ const AddressMap = () => {
       mapRef.current,
       "rightclick",
       function (e) {
-        console.log("rightClick");
+        console.log("rightClick, 좌표: ", e.coord);
         searchCoordinateToAddress(e.coord);
       }
     );
@@ -86,121 +80,82 @@ const AddressMap = () => {
         contextMenuWindowRef.current.close();
       }
 
-      // 모든 이벤트 리스너를 제거합니다.
-      //naver.maps.Event.clearInstanceListeners(mapRef.current);
+      // 우클릭 이벤트 리스너를 제거합니다.
       naver.maps.Event.removeListener(rightClick);
-      // 추가적으로 설정된 리스너가 있다면, 여기에서 clearListeners를 호출합니다.
     };
   }, [startTime, endTime, opportunityCost, subwayCost, busCost, walkingCost]);
 
-  // 나머지 함수들(searchCoordinateToAddress, searchAddressToCoordinate 등)은 여기에 정의...
+  //핸들러
   const handleAddressChange = (e) => {
     setAddress(e.target.value);
   };
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    searchAddressToCoordinate(address);
-  };
-
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       searchAddressToCoordinate(address);
     }
   };
 
-  //중요함수 가져오기
+  //우클릭 메뉴만들기 - 함수 내부를 쪼개는게 좋을듯
   function searchCoordinateToAddress(latlng) {
-    //infoWindowRef.current.close();
-
-    naver.maps.Service.reverseGeocode(
-      {
-        coords: latlng,
-        orders: [
-          naver.maps.Service.OrderType.ADDR,
-          naver.maps.Service.OrderType.ROAD_ADDR,
-        ].join(","),
-      },
-      function (status, response) {
-        if (status === naver.maps.Service.Status.ERROR) {
-          return alert("Something Wrong!");
-        }
-
-        var items = response.v2.results,
-          address = "",
-          htmlAddresses = [];
-
-        for (var i = 0, ii = items.length, item, addrType; i < ii; i++) {
-          item = items[i];
-          address = makeAddress(item) || "";
-          addrType = item.name === "roadaddr" ? "[도로명 주소]" : "[지번 주소]";
-
-          htmlAddresses.push(i + 1 + ". " + addrType + " " + address);
-        }
-
-        contextMenuWindowRef.current.setContent(
-          [
-            '<div style="padding:10px;min-width:100px;line-height:150%;">',
-            // '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
-            // htmlAddresses.join("<br />"),
-            '   <button id="startButton" >출발</button>',
-            '   <button id="endButton" >도착</button>',
-            "</div>",
-          ].join("\n")
-        );
-
-        contextMenuWindowRef.current.open(mapRef.current, latlng);
-
-        //버튼에 이벤트 리스너를 등록합니다.
-
-        const startButton = document.getElementById("startButton");
-        const endButton = document.getElementById("endButton");
-
-        if (startButton) {
-          startButton.addEventListener("click", () => {
-            contextMenuWindowRef.current.close();
-
-            if (startMarkerRef.current != null) {
-              startMarkerRef.current.setMap(null);
-            }
-
-            const marker = new naver.maps.Marker({
-              position: latlng,
-              map: mapRef.current,
-              icon: "./출발test.png",
-            });
-
-            startMarkerRef.current = marker;
-
-            //서버에요청
-            requestMinCostRoute();
-          });
-        }
-
-        if (endButton) {
-          endButton.addEventListener("click", () => {
-            contextMenuWindowRef.current.close();
-
-            if (endMarkerRef.current != null) {
-              endMarkerRef.current.setMap(null);
-            }
-
-            const marker = new naver.maps.Marker({
-              position: latlng,
-              map: mapRef.current,
-              icon: "./도착test.png",
-            });
-
-            endMarkerRef.current = marker;
-
-            //서버에요청
-            requestMinCostRoute();
-          });
-        }
-      }
+    contextMenuWindowRef.current.setContent(
+      [
+        '<div style="padding:10px;min-width:100px;line-height:150%;">',
+        '   <button id="startButton" >출발</button>',
+        '   <button id="endButton" >도착</button>',
+        "</div>",
+      ].join("\n")
     );
+    contextMenuWindowRef.current.open(mapRef.current, latlng);
+
+    //버튼에 이벤트 리스너를 등록합니다.
+    const startButton = document.getElementById("startButton");
+    const endButton = document.getElementById("endButton");
+
+    if (startButton) {
+      startButton.addEventListener("click", () => {
+        contextMenuWindowRef.current.close();
+
+        if (startMarkerRef.current != null) {
+          startMarkerRef.current.setMap(null);
+        }
+
+        const marker = new naver.maps.Marker({
+          position: latlng,
+          map: mapRef.current,
+          icon: "./출발test.png",
+        });
+
+        startMarkerRef.current = marker;
+        console.log("marker: ", marker);
+
+        //서버에요청
+        requestMinCostRoute();
+      });
+    }
+
+    if (endButton) {
+      endButton.addEventListener("click", () => {
+        contextMenuWindowRef.current.close();
+
+        if (endMarkerRef.current != null) {
+          endMarkerRef.current.setMap(null);
+        }
+
+        const marker = new naver.maps.Marker({
+          position: latlng,
+          map: mapRef.current,
+          icon: "./도착test.png",
+        });
+
+        endMarkerRef.current = marker;
+
+        //서버에요청
+        requestMinCostRoute();
+      });
+    }
   }
 
+  //주소로 좌표찾고 이동
   function searchAddressToCoordinate(address) {
     naver.maps.Service.geocode(
       {
@@ -212,12 +167,15 @@ const AddressMap = () => {
         }
 
         if (response.v2.meta.totalCount === 0) {
-          return alert("totalCount" + response.v2.meta.totalCount);
+          return alert(
+            "지역 명이나 도로명 주소를 입력해주세요\n" +
+              "ex) 인천, 부개동, 새벌로133번길21 "
+          );
         }
 
         var htmlAddresses = [],
           item = response.v2.addresses[0],
-          point = new naver.maps.Point(item.x, item.y);
+          point = new naver.maps.LatLng(item.y, item.x);
 
         if (item.roadAddress) {
           htmlAddresses.push("[도로명 주소] " + item.roadAddress);
@@ -242,92 +200,17 @@ const AddressMap = () => {
           ].join("\n")
         );
 
+        console.log("searchAddressToCoordinate, response: ", response);
+        console.log("searchAddressToCoordinate, point: ", point);
+
         mapRef.current.setCenter(point);
-        contextMenuWindowRef.current.open(mapRef.current, point);
+        searchCoordinateToAddress(point);
+        //contextMenuWindowRef.current.open(mapRef.current, point);
       }
     );
   }
 
-  //남은함수 그대로가져오기
-  function makeAddress(item) {
-    if (!item) {
-      return;
-    }
-
-    var name = item.name,
-      region = item.region,
-      land = item.land,
-      isRoadAddress = name === "roadaddr";
-
-    var sido = "",
-      sigugun = "",
-      dongmyun = "",
-      ri = "",
-      rest = "";
-
-    if (hasArea(region.area1)) {
-      sido = region.area1.name;
-    }
-
-    if (hasArea(region.area2)) {
-      sigugun = region.area2.name;
-    }
-
-    if (hasArea(region.area3)) {
-      dongmyun = region.area3.name;
-    }
-
-    if (hasArea(region.area4)) {
-      ri = region.area4.name;
-    }
-
-    if (land) {
-      if (hasData(land.number1)) {
-        if (hasData(land.type) && land.type === "2") {
-          rest += "산";
-        }
-
-        rest += land.number1;
-
-        if (hasData(land.number2)) {
-          rest += "-" + land.number2;
-        }
-      }
-
-      if (isRoadAddress === true) {
-        if (checkLastString(dongmyun, "면")) {
-          ri = land.name;
-        } else {
-          dongmyun = land.name;
-          ri = "";
-        }
-
-        if (hasAddition(land.addition0)) {
-          rest += " " + land.addition0.value;
-        }
-      }
-    }
-
-    return [sido, sigugun, dongmyun, ri, rest].join(" ");
-  }
-
-  function hasArea(area) {
-    return !!(area && area.name && area.name !== "");
-  }
-
-  function hasData(data) {
-    return !!(data && data !== "");
-  }
-
-  function checkLastString(word, lastString) {
-    return new RegExp(lastString + "$").test(word);
-  }
-
-  function hasAddition(addition) {
-    return !!(addition && addition.value);
-  }
-
-  //내가 추가한 함수들
+  //최소비용 경로와 시간 요청
   const requestMinCostRoute = () => {
     if (startMarkerRef.current != null && endMarkerRef.current != null) {
       setShowResult(true);
@@ -358,7 +241,10 @@ const AddressMap = () => {
         })
         .catch((error) => {
           console.error("Error:", error);
-          alert.error("Error:", error); // 오류 처리
+          alert(
+            "서버가 불안정합니다. 새로고침 후 다시 시도 해주세요. Error:" +
+              error.message
+          ); // 오류 처리
         });
     }
   };
@@ -430,18 +316,6 @@ const AddressMap = () => {
     const seconds = date.getSeconds().toString().padStart(2, "0");
     return `${hours}시 ${minutes}분 ${seconds}초`;
   }
-
-  const handleAddressChange2 = (e) => {
-    console.log(contextMenuWindowRef.current);
-    setAddress(e.target.value);
-  };
-
-  const myHandle = (e) => {
-    naver.maps.Marker({
-      positon: e.coord,
-      map: mapRef.current,
-    });
-  };
 
   return (
     <div>
